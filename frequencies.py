54623a4e29927f4bb3b20f9c560429ee062b8dbe
Armaan Uddin <armaan.uddin@kcl.ac.uk>
2023-10-13 20:17:11 +0100

    Add method to create a dictionary of how many times each item appears in a list

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,16 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
+    
     frequencies = {}
     # Your code goes here
+    for item in items:
+
+        key = str(item)
+        
+        if(key in frequencies):
+            frequencies[key] = frequencies[key] + 1
+        else:
+            frequencies[key] = 1
+
     return frequencies
